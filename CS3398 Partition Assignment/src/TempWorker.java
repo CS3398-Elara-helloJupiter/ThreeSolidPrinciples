4987319f4d8c39ff00891e0ea7263ab24eb24d44
Jessica <j_c545@txstate.edu>
2018-09-15 00:11:24 -0500

    Broke Worker file to three different files

---
+++ CS3398 Partition Assignment/src/TempWorker.java
@@ -0,0 +1,14 @@
+package threesolid;
+
+
+public class TempWorker extends BaseWorker implements IEat, ISick {
+	@Override
+	public void work() {
+		//.... working
+	}
+	public void eat() {
+		//.... eating in launch break
+	}
+	public void sick() {
+		//.... I'm sick
+	}
