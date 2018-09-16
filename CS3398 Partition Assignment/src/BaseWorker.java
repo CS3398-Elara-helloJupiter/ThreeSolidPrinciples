d8b5453b993f2c1177f4c47579c9582e53af8ae0
j-c545 <42955673+j-c545@users.noreply.github.com>
2018-09-15 20:13:41 -0500

    Added output statement

--- CS3398 Partition Assignment/src/BaseWorker.java
+++ CS3398 Partition Assignment/src/BaseWorker.java
@@ -3,7 +3,7 @@ package threesolid;
 public class BaseWorker {
 
 	public void work() {
-		System.out.format("Calling work function from the Base class \n"); 
-	}
+		System.out.format("Base Worker is working \n"); 
+	
 
 }
