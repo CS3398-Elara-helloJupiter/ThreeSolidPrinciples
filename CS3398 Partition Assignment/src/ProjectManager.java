24a73407f049e8309c34e64a7827284df149f342
smh196 <32583165+smh196@users.noreply.github.com>
2018-09-15 20:10:27 -0500

    Add manager subclasses

---
+++ CS3398 Partition Assignment/src/ProjectManager.java
@@ -0,0 +1,11 @@
+package threesolid;
+
+//Author: Samantha Hollensbe
+
+public class ProjectManager extends Manager {
+
+	public void scheduleworker()
+	{
+		System.out.format("Project Manager is scheduling worker \n");
+	}
+}
