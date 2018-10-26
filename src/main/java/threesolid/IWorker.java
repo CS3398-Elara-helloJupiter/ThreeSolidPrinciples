8da716cc51136e687f0351cffc77e116fb3fa5e7
AaronH <a_h230@txstate.edu>
2018-10-25 19:50:10 -0500

    plz work

---
+++ src/main/java/threesolid/IWorker.java
@@ -0,0 +1,15 @@
+package threesolid;
+
+/**
+ *Provides tagging interface for extension in BaseWorker classes
+ *@author Louis Scinta
+ *@version 09.15.18
+ *@since 09.14.18
+ */
+public interface IWorker 
+{}
+
+/*
+ *IWorker is a tagging interface to both act as a parent interface for IEat, ISick, IReboot;
+ *and to be used as a data type in classes (specifically BaseWorker and its children)
+*/
---
+++ src/main/java/threesolid/ProductManager.java
@@ -0,0 +1,11 @@
+package threesolid;
+
+//Author: Samantha Hollensbe
+
+public class ProductManager extends Manager {
+	
+	public void defineproduct()
+	{
+		System.out.format("You have called defineproduct... \n");
+	}
+}
