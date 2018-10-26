5cd3c9a2afa9a2f2114b4b85287c359e04e214ef
smh196 <32583165+smh196@users.noreply.github.com>
2018-10-26 17:23:46 -0500

    Assignment 13 Stuff

--- src/test/java/testthreesolid/TestWorker.java
+++ src/test/java/testthreesolid/TestWorker.java
@@ -4,12 +4,26 @@ package threesolid;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-public class TestWorker {
+public class TestWorker 
+{
  private Worker w = new Worker();
  @Test
  @DisplayName("Work Method Test")
  public void testWorkMethod()
  {
    assertEquals(w.work(),"I'm working already!", "Mismatch between test text and method text" );
+ }
+
+ @Test
+ @DisplayName("newtest_smh_WorkerPass")
+ public void newtest_smh_WorkerPass()
+ {
+ 	assertTrue(true==true, "It's Alive!!");
+ }
+ @Test
+ @DisplayName("newtest_smh_WorkerFail")
+ public void newtest_smh_WorkerFail()
+ {
+ 	assertTrue(true==false, "It's dead...");
  }
 }
