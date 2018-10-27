63dc796dd97c4dcebe31193a57c9d35adf955887
AaronH <a_h230@txstate.edu>
2018-10-27 17:05:43 -0500

    Re-Structured branch to work with Travis

---
+++ src/main/java/threesolid/ProjectManager.java
@@ -0,0 +1,11 @@
+package threesolid;
+
+//Author: Samantha Hollensbe
+
+public class ProjectManager extends Manager {
+
+	public void scheduleworker()
+	{
+		System.out.format("You have called scheduleworker... \n");
+	}
+}
